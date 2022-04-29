4470287b4d0ff5ba0d3e92cc6abb1be051ca5591
qveerty <96960171+qveerty@users.noreply.github.com>
2022-04-29 22:54:24 +0200

    Update and rename hello.c to test.py

--- hello.c
+++
@@ -1,8 +0,0 @@
-#include <iostream.h>
-
-int main(){
-  char name[20];
-  printf("\nWhat's your name? ");
-  scanf("%s", name);
-  printf("\nHello %s!\n\n", name);
-}
---
+++ test.py
@@ -0,0 +1,32 @@
+from typing import Union
+from math import sqrt
+from random import uniform, normalvariate
+
+
+def probability(pc: int, tc: int) -> Union[int, float]:
+    return (pc / tc) * 100
+
+
+def mean(numbers: list[Union[int, float]]) -> Union[int, float]:
+    return sum(numbers) / len(numbers)
+
+
+def variance(n: list[Union[int, float]]) -> Union[int, float]:
+    return mean([pow(number - mean(n), 2) for number in n])
+
+
+def std(array: list[Union[int, float]]) -> Union[int, float]:
+    return sqrt(variance(array))
+
+
+def generate_matrix(start: float, end: float, numbers: int) -> list:
+    matrix = []
+    while len(matrix) < numbers:
+        matrix.append(uniform(start, end))
+    return matrix
+
+
+def normal_distribution(m: Union[int, float], standard_deviation: float) -> float:
+    return normalvariate(m, standard_deviation)
+
+print(variance(generate_matrix(0.0, 5.0, 100000)))
